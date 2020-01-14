006f6cc2dcceac67b1cc58adb901017e5fd7705d
egebeyazit <59344206+egebeyazit@users.noreply.github.com>
2020-01-14 11:49:22 -0600

    Updated the default parameters, added lr1, lr2.

--- params.py
+++ params.py
@@ -1,20 +1,21 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=500, help="number of epochs of training")
+parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+parser.add_argument("--lr1", type=float, default=0.001, help="adam: learning rate")
+parser.add_argument("--lr2", type=float, default=0.002, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-parser.add_argument("--latent_dim", type=int, default=62, help="dimensionality of the latent space")
-parser.add_argument("--code_dim", type=int, default=4, help="latent code")
-parser.add_argument("--n_classes", type=int, default=10, help="number of classes for dataset")
-parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
+parser.add_argument("--latent_dim", type=int, default=5, help="dimensionality of the latent space")
+parser.add_argument("--code_dim", type=int, default=5, help="latent code")
+parser.add_argument("--n_classes", type=int, default=0, help="number of classes for dataset")
+parser.add_argument("--img_size", type=int, default=64, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=1, help="number of image channels")
 parser.add_argument("--sample_interval", type=int, default=800, help="interval between image sampling")
 opt = parser.parse_args([])
 
 # Loss weights
-lambda_cat = 1
-lambda_con = 0.1
+lambda_cat = 0
+lambda_con = 0.05
